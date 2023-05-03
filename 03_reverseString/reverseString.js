7232b6e76c2d09f785b46fde9dbec0b4367d2702
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-05-03 18:53:58 +0800

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    
+    let wordReverse = ""
 
+    for(let i = word.length - 1; i >= 0; i--) {
+        wordReverse += word[i]
+    }
+
+    return wordReverse;
 };
 
 // Do not edit below this line
