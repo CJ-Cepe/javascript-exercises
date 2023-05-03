a6c385e6c4b4c0335412354d1a5b937e9cf15233
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-05-03 22:59:38 +0800

    finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    //check if valid number - no negatives and string
+    if (typeof start != 'number' || typeof end != 'number' || start < 0 || end < 0) {
+        return 'ERROR'
+    }
 
+    //set start (smaller) and end (bigger) 
+    let tempA, tempB;
+    let total = 0;
+
+    if(start<end){
+        tempA = start
+        tempB = end
+    } else if (start > end){
+        tempB = start
+        tempA = end
+    }
+
+    for(let i = tempA; i <= tempB; i++) {
+        total += i;
+    }
+    return total
 };
 
 // Do not edit below this line
