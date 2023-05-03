38f5071fd00a9313c75856e0578f7a08ed8059d0
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-05-03 18:36:56 +0800

    finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(textToPrint, numOfTimes) {
 
+    if(numOfTimes>=0){
+        let currentString = ""
+
+        for (let i=0;i<numOfTimes;i++){
+            currentString +=textToPrint;
+        }
+        return currentString
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
