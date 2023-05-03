03f77b1e8a1a8123359ef41d7429c3e6bbf99088
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-05-03 21:14:31 +0800

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+    for(let i = 0; i < arguments.length; i++){
+        if(array.includes(arguments[i])) {
+            let index = array.indexOf(arguments[i])
+            array.splice(index, 1)
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
