63c13b87a5f4801cfeb4c0d18092dbb252bcf411
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-02 18:05:00 +0800

    solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(addends_1, addends_2) {
+  return addends_1 + addends_2;
 };
 
-const subtract = function() {
-	
+const subtract = function(minued, subtrahend) {
+	return minued - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(addends) {
+	return sumValue = addends.reduce((accumulator, currentValue)=>{
+    return add(accumulator, currentValue)
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  return product = factors.reduce((accumulator, currentValue)=>{
+    return accumulator * currentValue
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+  let tempValue = 1
+	for(; factor > 0; factor--){
+    tempValue *= factor 
+  }
+  return tempValue
 };
 
 // Do not edit below this line
