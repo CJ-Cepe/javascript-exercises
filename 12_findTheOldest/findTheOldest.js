1c42325ed3325d37c640bbc801369c5afa267559
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-02 22:00:55 +0800

    solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentOldestAge = 0
+    let currentOldestPerson
+    let date = new Date()
 
+    people.map(person => {
+        let age
+        if(person.yearOfDeath){
+            age = person.yearOfDeath - person.yearOfBirth
+        } else {
+            age = date.getFullYear() - person.yearOfBirth
+        }
+        
+        if (currentOldestAge < age) {
+            currentOldestAge = age
+            currentOldestPerson = person
+        }
+    })
+
+    console.log(currentOldestPerson.name)
+    return currentOldestPerson
 };
 
 // Do not edit below this line
