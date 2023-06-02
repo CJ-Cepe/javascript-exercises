cd59939e0b460315c49708ed0712e2a49f0bec5f
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-02 19:23:24 +0800

    solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num
 
+    if(num<0) return 'OOPS'
+
+    let currentValue = []
+    for(let i = 0; i <= num; i++){
+        if(i>1){
+            currentValue.push(currentValue[i-1] + currentValue[i-2])
+        } else if(i == 1){
+            currentValue.push(i)
+        } else {
+            currentValue.push(i)
+        }
+    }
+    return currentValue.pop()
 };
 
 // Do not edit below this line
