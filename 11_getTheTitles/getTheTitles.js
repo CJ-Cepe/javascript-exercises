78b38735bedc39a5a01bac90d27a09e23dc4ced9
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-02 19:40:39 +0800

    add new solution using map function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,16 @@
 const getTheTitles = function(books) {
+
+    /*
+    # SOLUTION 1 ---- using for each and push
     let bookTitles = []
     books.forEach(element => {
         bookTitles.push(element['title'])
     });
-    return bookTitles
+    return bookTitles 
+    */
+
+    // SOLUTION 2 --- using map
+    return books.map((book)=>{return book.title})
 };
 
 // Do not edit below this line
