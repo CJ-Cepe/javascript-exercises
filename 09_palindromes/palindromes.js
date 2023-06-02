fa9236c787055a188cbd127faee4eb8878f4998b
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-02 18:33:33 +0800

    solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    //remove special characters, inlucing space
+    phrase = phrase.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    //reverse string then compare to original
+    reversePhrase = Array.from(phrase).reverse().join('')
+    return phrase == reversePhrase ? true : false;
 };
 
 // Do not edit below this line
